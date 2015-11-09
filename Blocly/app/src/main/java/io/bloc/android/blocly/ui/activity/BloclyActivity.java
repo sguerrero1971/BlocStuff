dc837c9ec67f9c0ffa8cfc2a198efee293da0c5b
Sergio Guerrero <sguerrero1971@gmail.com>
2015-11-09 08:49:42 -0500

    Checkpoint 33 Completed

---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -0,0 +1,19 @@
+package io.bloc.android.blocly.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import io.bloc.android.blocly.R;
+
+/**
+ * Created by SolarUser on 11/9/2015.
+ */
+public class BloclyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
+    }
+
+}
