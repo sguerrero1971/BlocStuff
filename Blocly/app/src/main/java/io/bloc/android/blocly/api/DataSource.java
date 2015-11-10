418a3cd803da39a2a1ff16efdf350e45838d8745
Sergio Guerrero <sguerrero1971@gmail.com>
2015-11-10 13:35:56 -0500

    assignment 41 completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/api/DataSource.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/api/DataSource.java
@@ -3,6 +3,8 @@ package io.bloc.android.blocly.api;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.bloc.android.blocly.BloclyApplication;
+import io.bloc.android.blocly.R;
 import io.bloc.android.blocly.api.model.RssFeed;
 import io.bloc.android.blocly.api.model.RssItem;
 
@@ -33,7 +35,12 @@ public class DataSource {
                 "This feed is just incredible, I can't even begin to tell you…",
                 "http://favoritefeed.net", "http://feeds.feedburner.com/favorite_feed?format=xml"));
         for (int i = 0; i < 10; i++) {
-            items.add(new RssItem(String.valueOf(i), "An incredible news story #" + i, "You won't believe how exciting this news story is, get ready to be blown away by its amazingness.", "http://favoritefeed.net?story_id=an-incredible-news-story", "http://rs1img.memecdn.com/silly-dog_o_511213.jpg", 0, System.currentTimeMillis(), false, false, false));
+            items.add(new RssItem(String.valueOf(i),
+                    BloclyApplication.getSharedInstance().getString(R.string.placeholder_headline) + " " + i,
+                    BloclyApplication.getSharedInstance().getString(R.string.placeholder_content),
+                    "http://favoritefeed.net?story_id=an-incredible-news-story",
+                    "http://wac.450f.edgecastcdn.net/80450F/tri1025.com/files/2012/03/iStockphotodog-630x472.jpg",
+                    0, System.currentTimeMillis(), false, false, false));
         }
     }
 }
