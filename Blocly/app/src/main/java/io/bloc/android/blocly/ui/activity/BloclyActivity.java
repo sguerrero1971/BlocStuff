1b1be50b1343b74549dbee4e16c00a510cd211ea
sguerrero1971 <sguerrero1971@gmail.com>
2015-11-28 19:29:00 -0500

    assignment 57 completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -61,7 +61,8 @@ public class BloclyActivity extends ActionBarActivity implements NavigationDrawe
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                BloclyApplication.getSharedDataSource().fetchNewFeed("http://feeds.feedburner.com/androidcentral?format=xml",
+                //  BloclyApplication.getSharedDataSource().fetchNewFeed("http://feeds.feedburner.com/androidcentral?format=xml",
+                BloclyApplication.getSharedDataSource().fetchNewFeed("http://feeds.ign.com/ign/all?format=xml",
                         new DataSource.Callback<RssFeed>() {
                             @Override
                             public void onSuccess(RssFeed rssFeed) {
